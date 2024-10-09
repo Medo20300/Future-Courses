28ec1e7bc09e69cb734673d279bd889659fe5dfc
Medo20300 <medhatconcrete@gmail.com>
2024-10-10 00:51:08 +0300

    Updatedir and files

---
+++ create_app.py
@@ -0,0 +1,7 @@
+from pythonic.models import db
+from pythonic import create_app
+
+app = create_app()
+with app.app_context():
+    db.create_all()
+print('Database created successfully')
---
+++ run.py
@@ -0,0 +1,7 @@
+from pythonic import create_app
+from flask import app
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
+   
