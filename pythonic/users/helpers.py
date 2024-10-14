5d83e72972a6f22f310698824d0c4534854bbd93
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-10-14 18:16:20 +0300

    send reset email file

--- pythonic/users/helpers.py
+++ pythonic/users/helpers.py
@@ -1,17 +1,19 @@
 from pythonic import mail
-from flask_mail import Message
+from flask_mail import Message # Import Flask-Mail's Message class to send emails
 from flask import url_for
 
 
 def send_reset_email(user):
-    token = user.get_reset_token()
+    """Function to send a password reset email to the user"""
+
+    token = user.get_reset_token() # Generate a password reset token for the user
     msg = Message(
         "Pythonic App Password Reset Request",
         sender="ElearnApp.project@gmail.com",
-        recipients=[user.email],
+        recipients=[user.email],  # The recipient's email address
         body=f"""To reset your password, visit the following link:
         {url_for('users.reset_password', token=token, _external=True)}
         
         if you did not make this request, please ignore this email.""",
     )
-    mail.send(msg)
+    mail.send(msg)  # Send the email using Flask-Mail's `send` function
