cf084b0fd92385e83280866c17fb7dcfb440fbb4
Medhat Mohamed Deif <135258669+Medo20300@users.noreply.github.com>
2024-10-14 16:06:33 +0300

    Run the application with debugging enabled and listening on port

--- run.py
+++ run.py
@@ -1,7 +1,12 @@
+
+# Import the application creation function from the main package
 from pythonic import create_app
+# Import the Flask app object
 from flask import app
+# Create an instance of the Flask application
 app = create_app()
 
+# Run the application with debugging enabled and listening on port 5000
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
    
