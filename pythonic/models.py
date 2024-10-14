9dd1acdb0065e7601835dd3a6723de831c9f6c54
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-10-14 21:05:48 +0300

    show user name

--- pythonic/models.py
+++ pythonic/models.py
@@ -28,6 +28,7 @@ class User(db.Model, UserMixin):
     fname = db.Column(db.String(25), nullable=False)
     lname = db.Column(db.String(25), nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
+    permission = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(125), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default="default.png")
     bio = db.Column(db.Text, nullable=True)
