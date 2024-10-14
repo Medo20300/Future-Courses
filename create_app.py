00c42a4c8f5addb790d6ddb21a43d9805491571d
Medhat Mohamed Deif <135258669+Medo20300@users.noreply.github.com>
2024-10-14 19:35:11 +0300

    Update create_app.py

--- create_app.py
+++ create_app.py
@@ -1,13 +1,7 @@
-# Import the database instance from the models module
-from pythonic.models import db
+from pythonic import db
+from pythonic import create_app  
 
-
-from pythonic import create_app
-# Create an instance of the Flask application
-app = create_app()
-# Establish the application context and create the database schema
-with app.app_context():
-    db.create_all()
-
-# Print confirmation message once the database is created successfully
-print('Database created successfully')
+app = create_app()  
+with app.app_context():  
+    db.create_all()  
+print('Database created successfully') 
