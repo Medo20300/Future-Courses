9d76aa2d2bc857db2786f81e1e51ae889dffb415
Medo20300 <medhatconcrete@gmail.com>
2024-10-16 16:13:54 +0300

    Create app instance and run

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ app = create_app()
 
 # Run the application with debugging enabled and listening on port 5000
 if __name__ == "__main__":
-    app.run(debug=True,host= '0.0.0.0" port=5000)
+    app.run(debug=True, port=5000)
    
