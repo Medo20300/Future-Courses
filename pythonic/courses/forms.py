143d4ef2746dc8ca3ea34e9a6e640d03764939a0
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-10-21 18:08:43 +0300

    update courses forms

--- pythonic/courses/forms.py
+++ pythonic/courses/forms.py
@@ -1,15 +1,18 @@
 from flask_wtf import FlaskForm
-from flask_wtf.file import FileField, FileAllowed
+from flask_wtf.file import FileField, FileAllowed  # Importing FlaskForm to create forms, and FileField to handle file uploads
 from pythonic.models import Course
 from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import (
-    DataRequired,
-    Length,
-    ValidationError,
+    DataRequired, # DataRequired ensures that a field is not left empty. 
+    Length,  # Length limits the character length of input, 
+    ValidationError,  # and ValidationError is used to raise custom validation errors.
+
 )
 
 
 class NewCourseForm(FlaskForm):
+    """Defining the form for creating a new course"""
+
     title = StringField("Course Name", validators=[DataRequired(), Length(max=50)])
     description = TextAreaField(
         "Course Description", validators=[DataRequired(), Length(max=150)]
