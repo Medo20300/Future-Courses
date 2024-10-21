82efdfa1f3db0f12fbc6b9678c7377efa7ee283c
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-10-21 18:18:06 +0300

    update courses routes

--- pythonic/courses/routes.py
+++ pythonic/courses/routes.py
@@ -13,9 +13,14 @@ courses_bp = Blueprint("courses", __name__)
 
 @courses_bp.route("/<string:course_title>")
 def course(course_title):
+    # Query the Course model to find a course by its title.
     course = Course.query.filter_by(title=course_title).first()
+
+     # If a course is found, store its ID; otherwise, set course_id to None.
     course_id = course.id if course else None
     course = Course.query.get_or_404(course_id)
+
+    # Get the page number from the URL query parameters. Defaults to page 1 if not specified.
     page = request.args.get("page", 1, type=int)
     lessons = Lesson.query.filter_by(course_id=course_id).paginate(
         page=page, per_page=6
@@ -30,6 +35,11 @@ def course(course_title):
 
 @courses_bp.route("/courses")
 def courses():
+    """courses main route"""
+
+     # Get the page number from the URL query parameters. Defaults to page 1 if not specified.
     page = request.args.get("page", 1, type=int)
+
+    # Query the Course model to paginate and display 6 courses per page.
     courses = Course.query.paginate(page=page, per_page=6)
     return render_template("courses.html", title="Courses", courses=courses)
